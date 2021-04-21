7cc9fe8fda892a4318ffd14cd93e7695f0061a83
Shreyas Chittlur Ramesh <chittlurramesh.s@northeastern.edu>
2021-04-21 14:01:23 -0400

    Skeleton with Enterprise and Organization

---
+++ src/Business/Employee/Employee.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Employee;
+
+import Business.Doctor.DoctorDirectory;
+
+/**
+ *
+ * @author raunak
+ */
+public class Employee {
+    
+    private String name;
+    private String city;
+    
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    private int id;
+    private static int count = 1;
+
+    public Employee() {
+        
+        id = count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    
+}
